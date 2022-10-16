76d701a2d412d426cc180077ad564f82d830cebf
facuferreria <ferreriafacundo@gmail.com>
2022-10-15 22:46:34 -0300

    Cambios Responsive

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -2,18 +2,31 @@ import React from 'react'
 import CartWidget from '../CartWidget/CartWidget'
 import './NavBar.scss'
 import { NavLink } from 'react-router-dom'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
 
 function NavBar() {
+
   return (
-    <div className="nav">
-        <NavLink to= "/" className="nav-title"><p>BROOK SHOP</p></NavLink>
-        <NavLink to= "/category/deportivo" className="nav-option"><p>DEPORTIVO</p></NavLink>
-        <NavLink to= "/category/salida" className="nav-option"><p>PARA SALIR</p></NavLink>
-        <NavLink to= "/category/cuero" className="nav-option"><p>DE CUERO</p></NavLink>
-        <NavLink to= "/nosotros" className="nav-option"><p>SOBRE NOSOTROS</p></NavLink>
-        <NavLink to= "/contacto" className="nav-option"><p>CONTACTENOS</p></NavLink>
-        <NavLink to= "/cart" className="nav-widget"><CartWidget /></NavLink>
-    </div>
+  <div>
+    <Navbar collapseOnSelect expand="lg"  variant="light">
+      <Container>
+        <NavLink to= "/" className="navbar-title"><p>BROOK SHOP</p></NavLink>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <NavLink to= "/category/deportivo" className="navbar-option"><p>DEPORTIVO</p></NavLink>
+            <NavLink to= "/category/cuero" className="navbar-option"><p>DE CUERO</p></NavLink>
+            <NavLink to= "/contacto" className="navbar-option"><p>CONTACTENOS</p></NavLink>
+            <NavLink to= "/nosotros" className="navbar-option"><p>SOBRE NOSOTROS</p></NavLink>
+            <NavLink to= "/cart" className="navbar-widget"><CartWidget /></NavLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  </div>
   )
 }
 
